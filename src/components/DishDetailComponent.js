b5d90f820100ecd8cd4caa8b183f6debc59b5509
Karthik Gangadharaiah <Karthik@Karthiks-MacBook-Pro.local>
2020-09-17 14:31:57 -0500

    Single page applications - part 2 - 09/17/2020

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
     if (dish != null) {
         return(
-            <Card className="col-12 col-md-5 m-1">
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
         );
     } else {
         return(
@@ -19,34 +22,22 @@ function RenderDish({dish}) {
     }
 }
 
-function RenderComments({dish}) {
-    if(dish != null){
-        if(dish.comments != null){
-            if (dish.comments.length !== 0) {
-                return (
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        {dish.comments.map(comment => (
-                            <ul className="list-unstyled">
-                                <li>
-                                    <p>{comment.comment}</p>
-                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                            </ul>
-                        )
-                        )}
-                    </div>
-                );
-            } else {
-                return (
-                    <div></div>
-                );
-            }
-        }
-    } else {
+function RenderComments({comments}) {
+    if(comments != null){
         return (
-            <div></div>
-        )
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map(comment => (
+                    <ul className="list-unstyled">
+                        <li>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                        </li>
+                    </ul>
+                )
+                )}
+            </div>
+        );
     }
 }
 
@@ -54,8 +45,18 @@ const DishDetail = (props) => {
     return (
         <div className="container">
             <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="'/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr></hr>
+                </div>
+            </div>
+            <div className="row">
                 <RenderDish dish={props.dish}/>
-                <RenderComments dish={props.dish}/>
+                <RenderComments comments={props.comments}/>
             </div>
         </div>
     )
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import DishDetail from './DishDetailComponent';
 import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -32,6 +33,14 @@ class Main extends Component {
                 />
             );
         }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                />
+            );
+        }
         
 		return (
 			<div>
@@ -39,6 +48,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
